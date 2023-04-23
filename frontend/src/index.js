8069bb9028fab8643b2b5eb9aeb3862af46c0e39
johnpennyman95@gmail.com <john.pennyman@okstate.edu>
2023-04-22 19:36:17 -0500

    fixes some file sources

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from './App'
-import Users from "./components/Users"
 
-ReactDOM.createRoot(document.getElementById('root')).render(<div><App /><Users /></div>)
+ReactDOM.createRoot(document.getElementById('root')).render(<div><App /></div>)
